3d76cd4e72fdd2b3854942c7fb99b4011bf6e50a
陈光明 <chemist_visa@163.com>
2018-09-17 00:14:56 +0800

    fix to multi

--- myblock.c
+++ myblock.c
@@ -18,18 +18,29 @@
 #include <asm/dma.h>
 #include <asm-generic/errno-base.h>
 
+#define MAX_DEV_NAME_LEN (127)
+#define MAX_DEVICE_COUNT (31)
+#define BYTE_PER_SECTOR (512)
 
-#define RAMBLOCK_SIZE (300 * 1024 * 1024)
+//设备信息
 #define HEAD_CNT (64)
 #define SECTOR_CNT (64)
-#define BYTE_PER_SECTOR (512)
-#define CYLINDER_CNT (RAMBLOCK_SIZE / BYTE_PER_SECTOR / HEAD_CNT / SECTOR_CNT)
 
-static struct gendisk *ram_block;
-static struct request_queue *ram_block_queue;
-static unsigned char *ramblock_buf; 
-static int major;
-static DEFINE_SPINLOCK(ramblock_lock);
+
+typedef struct dev_info_s{
+	struct hd_geometry geo ;					// 设备信息，size
+	char dev_name[MAX_DEV_NAME_LEN];			// 设备名字，显示的blockname
+	char * buffer ;								// 内存起始地址
+	DEFINE_SPINLOCK(ramblock_lock);				// 操作自旋锁
+	struct gendisk * ram_block ;				//
+	struct request_queue * ram_block_queue ;	// 消息队列
+	int major ;									// 
+	int device_size ;							// 设备大小，sector
+	short in_use:1 ;
+}dev_info_t;
+
+dev_info_t g_devs[MAX_DEVICE_COUNT] ;
+int g_dev_cnt ;
  
 static void do_ramblock_request(struct request_queue * q){
 	struct request *req;
@@ -59,7 +70,7 @@ static int ramblock_getgeo(struct block_device *bdev, struct hd_geometry *geo)
 	/* 容量=heads*cylinders*sectors*512 */
 	geo->heads     = HEAD_CNT ;
 	geo->cylinders = CYLINDER_CNT ;
-	geo->sectors   = SECTOR_CNT ;
+	geo->sectors   = g_devs[g_dev_cnt].device_size / HEAD_CNT / SECTOR_CNT ;
 	return 0;
 }
  
@@ -67,23 +78,29 @@ static struct block_device_operations ram_block_fops={
 	.owner	= THIS_MODULE,
 	.getgeo	= ramblock_getgeo,
 };
-static int ram_block_int(void){
-	//ramblock_buf = kzalloc(RAMBLOCK_SIZE, GFP_KERNEL);
-	ramblock_buf = vmalloc(RAMBLOCK_SIZE);
+
+//创建一个block_size（MB）大小的RAM based disk
+static int create_ram_block(int block_size){
+	dev_info_t * device = &devs[g_dev_cnt] ;
+	ramblock_buf = vmalloc(block_size * 1024 * 1024);
 	if(NULL == ramblock_buf){
 		return -ENOMEM;
 	}
-
-	ram_block=alloc_disk(5);
-	ram_block_queue=blk_init_queue(do_ramblock_request, &ramblock_lock);
-	ram_block->queue=ram_block_queue;
-	major=register_blkdev(0,"ram_block");
-	ram_block->major=major;
-	ram_block->first_minor=0;
-	ram_block->fops=&ram_block_fops;
-	sprintf(ram_block->disk_name,"ram_block");
-	set_capacity(ram_block, RAMBLOCK_SIZE / 512);
-	add_disk(ram_block);
+	device->device_size = block_device * 1024 * 2 ;
+	device->ram_block = alloc_disk(5);
+	device->ram_block_queue = blk_init_queue(do_ramblock_request, &device->ramblock_lock);
+	device->ram_block->queue=ram_block_queue;
+	device->major=register_blkdev(0,device->dev_name);
+	device->ram_block->major=device->major;
+	device->ram_block->first_minor=0;
+	device->ram_block->fops=&ram_block_fops;
+	sprintf(ram_block->disk_name,device->dev_name);
+	set_capacity(device->ram_block, device->device_size);
+	add_disk(device->ram_block);
+	
+}
+static int ram_block_int(void){
+	g_dev_cnt = 0 ;	
 	return	0 ;
 }
  
